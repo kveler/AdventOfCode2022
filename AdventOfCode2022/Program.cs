2abba7ddcb3af02adf2e091544b6bc3802cb8570
Max Veenstra <max_veenstra3@msn.com>
2022-12-01 00:58:48 +0100

    Prepare for day 1

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, welcome to the Advent of Code 2022 solution program of Kveler\n\n");
+Console.Write("Select a day: ");
+string day = Console.ReadLine();
+switch (day)
+{
+    case "1":
+        Console.WriteLine("Work in progress");
+        break;
+    default:
+        Console.WriteLine("That day has not been implemented yet, pick a different day");
+        break;
+}
