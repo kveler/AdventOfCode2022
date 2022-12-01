e2da89c3961ee50ff99add03651c1721656f057e
Max Veenstra <max_veenstra3@msn.com>
2022-12-01 12:17:38 +0100

    Update Day 1

---
+++ AdventOfCode2022/Day1Solver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    internal class Day1Solver
+    {
+        public void Day1()
+        {
+            slowLog("Reading data from website");
+            string data = File.ReadAllText(@"C:\Users\max_v\source\repos\AdventOfCode2022\AdventOfCode2022\Data\Day1Data.txt");
+
+            slowLog("Extracting data to array");
+            string[] lineData = data.Split(
+                new string[] { Environment.NewLine },
+                StringSplitOptions.None
+            );
+
+            slowLog("Calculating sums of elves callories");
+            List<int> elves = new List<int>();
+            int intake = 0;
+            foreach (string line in lineData)
+            {
+                if (line != "")
+                {
+                    intake += int.Parse(line);
+                }
+                else
+                {
+                    elves.Add(intake);
+                    intake = 0;
+                }
+            }
+
+            slowLog("Finding elve with most callories");
+            elves.Sort();
+            elves.Reverse();
+
+            slowLog("Elf with most callories has: " + elves.First() + " callories");
+            slowLog("Updating task: Find sum of top 3 highest callorie elves");
+
+            int top3Total = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                top3Total += elves[i];
+            }
+
+            slowLog("Total of top 3 elves: " + top3Total);
+        }
+        static void slowLog(string log)
+        {
+            Console.WriteLine(log);
+
+            Random rnd = new Random();
+            Thread.Sleep(rnd.Next(1000, 3000));
+        }
+    }
+}
--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using AdventOfCode2022;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -8,60 +10,12 @@
         {
             case "1":
                 Console.Clear();
-                Day1();
+                Day1Solver day1 = new Day1Solver();
+                day1.Day1();
                 break;
             default:
                 Console.WriteLine("That day has not been implemented yet, pick a different day");
                 break;
-        }
-
-        static void Day1()
-        {
-            slowLog("Reading data from website");
-            string data = File.ReadAllText(@"C:\Users\max_v\source\repos\AdventOfCode2022\AdventOfCode2022\Data\Day1Data.txt");
-
-            slowLog("Extracting data to array");
-            string[] lineData = data.Split(
-                new string[] { Environment.NewLine },
-                StringSplitOptions.None
-            );
-
-            slowLog("Calculating sums of elves callories");
-            List<int> elves = new List<int>();
-            int intake = 0;
-            foreach (string line in lineData)
-            {
-                if (line != "")
-                {
-                    intake += int.Parse(line);
-                }
-                else
-                {
-                    elves.Add(intake);
-                    intake = 0;
-                }
-            }
-
-            slowLog("Finding elve with most callories");
-            elves.Sort();
-            elves.Reverse();
-
-            slowLog("Elf with most callories has: " + elves.First() + " callories");
-            slowLog("Updating task: Find sum of top 3 highest callorie elves");
-
-            int top3Total = 0;
-            for(int i = 0; i < 3; i++)
-            {
-                top3Total += elves[i];
-            }
-
-            slowLog("Total of top 3 elves: " + top3Total);
-        }
-
-        static void slowLog(string log)
-        {
-            Console.WriteLine(log);
-            Thread.Sleep(2000);
         }
     }
 }
